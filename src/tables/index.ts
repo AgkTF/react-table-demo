cb63884b8e0f670426a26d73b1fa7b25bf2cce5e
Ahmed Gamal <agamal@raisaenergy.com>
2022-11-09 15:56:31 +0200

    -	renamed:    src/tables/DsTable/DsTable.tsx -> src/tables/BulkDSUTables/DsVerifiedTable.tsx

--- src/tables/index.ts
+++ src/tables/index.ts
@@ -1,4 +1,4 @@
 export * from './TractsTable/TractsTable';
 export * from './TractsTable/DSTractsTable';
 export * from './BulkDSUTables';
-export * from './DsTable/DsTable';
+export * from './BulkDSUTables/DsVerifiedTable';
